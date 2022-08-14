a3e60881aff8295e05683468d1442c6452600c0d
nbahire <n.bahire31@gmail.com>
2022-08-14 15:28:03 +0200

    add global model and controller

--- web/public/index.php
+++ web/public/index.php
@@ -1,15 +1,11 @@
 <?php
 
-include '../app/vendor/autoload.php';
-$foo = new App\Acme\Foo();
+use App\Acme\Core\Main;
+$_SESSION['erreur']= null;
+define('ROOT', dirname(__DIR__));
 
-?><!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Docker <?php echo $foo->getName(); ?></title>
-    </head>
-    <body>
-        <h1>Docker <?php echo $foo->getName(); ?></h1>
-    </body>
-</html>
+include '../app/vendor/autoload.php';
+require_once ROOT . '/app/vendor/autoload.php';
+
+$app = new Main();
+$app->start();

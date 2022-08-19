81abb7d788e6809200da4e9143843e0dfa2d1466
nbahire <n.bahire31@gmail.com>
2022-08-19 23:03:30 +0200

    Add principles templates

---
+++ web/app/src/Models/UsersModel.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Acme\Models;
+
+class UsersModel extends Model
+{
+    protected $id;
+    protected $name;
+    protected $email;
+    protected $password;
+    protected $roles;
+
+    public function __construct()
+    {
+
+        $this->table = 'users';
+    }
+
+    public function findByEmail(string $email)
+    {
+        return $this->requete("SELECT * FROM {$this->table} WHERE email = ?", [$email])->fetch();
+    }
+    /**
+     * Crée la session de l'utilisateur
+     *
+     * @return void
+     */
+    public function setSession(): void
+    {
+        $_SESSION['user'] = [
+            'id' => $this->id,
+            'email' => $this->email,
+            'roles' => $this->roles
+        ];
+    }
+
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of roles
+     */
+    public function getRoles()
+    {
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * Set the value of roles
+     *
+     * @return  self
+     */
+    public function setRoles($roles)
+    {
+        $this->roles = json_decode($roles);
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
